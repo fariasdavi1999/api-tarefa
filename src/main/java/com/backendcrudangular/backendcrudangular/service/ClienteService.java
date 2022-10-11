1924a29407fb9ff8117dae0b917f5a920ae12d1f
fariasdavi1999 <you@example.com>
2022-10-11 17:36:41 -0300

    comneçando estudar java security

---
+++ src/main/java/com/backendcrudangular/backendcrudangular/service/ClienteService.java
@@ -0,0 +1,47 @@
+package com.backendcrudangular.backendcrudangular.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.backendcrudangular.backendcrudangular.model.Cliente;
+import com.backendcrudangular.backendcrudangular.repository.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public List<Cliente> listarTodos() {
+
+        return clienteRepository.findAll();
+
+    }
+
+    public Optional<Cliente> listarPorId(Long id) {
+
+        return clienteRepository.findById(id);
+
+    }
+
+    public Cliente salvar(Cliente cliente) {
+
+        return clienteRepository.save(cliente);
+
+    }
+
+    public void deletar(Long id) {
+        clienteRepository.deleteById(id);
+    }
+
+    public Cliente alterar(Long id, Cliente cliente) {
+
+        cliente.setId(id);
+        return clienteRepository.save(cliente);
+
+    }
+
+}
---
+++ src/test/java/com/backendcrudangular/backendcrudangular/BackendCrudAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendcrudangular.backendcrudangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCrudAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
