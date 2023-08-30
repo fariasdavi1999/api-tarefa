a2a42b639beb9cd3ce449c22dc951d3523287b71
daviFNascimento <>
2023-08-30 15:19:04 -0300

    alterando corsconfig

--- src/main/java/com/backendcrudangular/backendcrudangular/config/CorsConfig.java
+++ src/main/java/com/backendcrudangular/backendcrudangular/config/CorsConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
@@ -18,67 +19,23 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @EnableScheduling
-public class CorsConfig extends CorsFilter {
+public class CorsConfig implements WebMvcConfigurer  {
 
 
 
-//		@Autowired
-//		private Environment environment;
-	//
-//		private String urlOrigem = environment.getProperty("spring.application.url.origem");
-
-		@Value("${spring.profiles.active}")
-		private String activeProfile;
-
-		public CorsConfig() {
-
-			super(configurationSource());
-		}
-
-    @Bean
-    WebMvcConfigurer corsConfigurer() {
-			return new WebMvcConfigurer() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-
-					CorsRegistration cors = registry.addMapping("/**");
-					cors.allowedMethods("GET", "PUT", "POST", "DELETE", "OPTIONS");
-					if (activeProfile.equals("local")) {
-						cors.allowedOrigins("http://localhost:4200");
-						
-					} else {
-
-					cors.allowedOrigins("https://primeiro-frontend-angular.vercel.app"); //prodFake
-//					cors.allowedOrigins("http://localhost:4200");
-
-					}
-					cors.allowedHeaders("*");
-				}
-			};
-		}
-
-		private static UrlBasedCorsConfigurationSource configurationSource() {
-
-			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-			CorsConfiguration config = new CorsConfiguration();
-			config.setAllowCredentials(true);
-
-//			config.addAllowedOrigin("https://primeiro-frontend-angular.vercel.app"); //prodFake
-			config.addAllowedOrigin("http://localhost:4200"); // local
-
-
-			config.addAllowedHeader("*");
-			config.addAllowedMethod("OPTIONS");
-			config.addAllowedMethod("HEAD");
-			config.addAllowedMethod("GET");
-			config.addAllowedMethod("PUT");
-			config.addAllowedMethod("POST");
-			config.addAllowedMethod("DELETE");
-			config.addAllowedMethod("PATCH");
-			config.addAllowedMethod("CONNECT");
-			source.registerCorsConfiguration("/**", config);
-			return source;
-		}
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+				.allowedOrigins("*")
+				.allowedMethods(
+						HttpMethod.GET.name(),
+						HttpMethod.POST.name(),
+						HttpMethod.PUT.name(),
+						HttpMethod.OPTIONS.name(),
+						HttpMethod.DELETE.name()
+				)
+				.allowedHeaders("*");
+	}
 
 	}
 	
