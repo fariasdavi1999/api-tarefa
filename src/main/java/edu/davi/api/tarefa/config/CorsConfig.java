9eb75def98d7f42857e5cf688c97d8302454366c
fariasdavi1999 <fariasdavi1999@gmail.com>
2024-08-11 16:59:13 -0300

    refactor: retirando imports e anotações não utilizadas

--- src/main/java/edu/davi/api/tarefa/config/CorsConfig.java
+++ src/main/java/edu/davi/api/tarefa/config/CorsConfig.java
@@ -1,24 +1,11 @@
 package edu.davi.api.tarefa.config;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.stereotype.Component;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
-@EnableScheduling
 public class CorsConfig implements WebMvcConfigurer  {
 
 
