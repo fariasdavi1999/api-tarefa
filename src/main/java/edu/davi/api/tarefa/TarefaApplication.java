e68046cbe3a1d9f46ae57357adc73685fe6833ff
fariasdavi1999 <fariasdavi1999@gmail.com>
2024-08-13 18:29:55 -0300

    refactor: adicionando atualização do spring doc swagger,

--- src/main/java/edu/davi/api/tarefa/TarefaApplication.java
+++ src/main/java/edu/davi/api/tarefa/TarefaApplication.java
@@ -1,9 +1,13 @@
 package edu.davi.api.tarefa;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Gerenciamento de Tarefas",
+                                description = "API de gerenciamento de Tarefas para Funcionários"))
 public class TarefaApplication {
 
 	public static void main(String[] args) {
