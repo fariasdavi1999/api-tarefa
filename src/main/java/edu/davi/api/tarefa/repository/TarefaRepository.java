2b4c89d63f8e7c7000cfd516bc548572a45b5b4b
fariasdavi1999 <fariasdavi1999@gmail.com>
2024-08-20 19:46:29 -0300

    refactor: removendo import

--- src/main/java/edu/davi/api/tarefa/repository/TarefaRepository.java
+++ src/main/java/edu/davi/api/tarefa/repository/TarefaRepository.java
@@ -3,7 +3,6 @@ package edu.davi.api.tarefa.repository;
 import edu.davi.api.tarefa.model.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Set;
 
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
