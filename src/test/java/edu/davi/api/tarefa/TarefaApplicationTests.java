e4166e15ba59084b5957419bc7e05e84dd4dbcc7
fariasdavi1999 <fariasdavi1999@gmail.com>
2026-01-14 15:37:21 -0300

    feat: configurando H2 database para aplicação e testes

--- src/test/java/edu/davi/api/tarefa/TarefaApplicationTests.java
+++ src/test/java/edu/davi/api/tarefa/TarefaApplicationTests.java
@@ -2,12 +2,24 @@ package edu.davi.api.tarefa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TarefaApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertDoesNotThrow(() -> {
+			// Verifica se o contexto da aplicação carrega corretamente
+		});
+	}
+
+	@Test
+	void mainMethodRuns() {
+		assertDoesNotThrow(() -> TarefaApplication.main(new String[]{}));
 	}
 
 }
